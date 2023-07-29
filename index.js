fb1d59dd985505318c65c56c253cc13f7fdb7d9b
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-07-29 05:46:33 +0300

    server running

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose from "mongoose";
+import mongoose, { Mongoose } from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
@@ -31,4 +31,23 @@ app.use("/general", generalRoutes);
 app.use("/management", managementRoutes);
 app.use("/sales", salesRoutes);
 
-/* --------------- https://youtu.be/0cPCMIuDk2I?list=LL&t=1089 -------------- */
+/* -------------------------------------------------------------------------- */
+/*                               Mongoose setup                               */
+/* -------------------------------------------------------------------------- */
+const PORT = process.env.PORT || 9000;
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`Did not connect: ${error.message}`);
+  });
+
+/* --------------- https://youtu.be/0cPCMIuDk2I?list=LL&t=1242 -------------- */
