20e5f7fe889ba9c643ad7d349afeb5aed25d1893
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-08-15 17:53:12 +0300

    data-for-product-productStat

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ import salesRoutes from "./routes/sales.js";
 
 /* ------------------------------ Data Imports ------------------------------ */
 import User from "./models/User.js";
-import { dataUser } from "./data/index.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import { dataUser, dataProduct, dataProductStat } from "./data/index.js";
 
 /* -------------------------------------------------------------------------- */
 /*                               Configurations                               */
@@ -49,6 +51,8 @@ mongoose
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
       /* ------------------------------ Only One Time ----------------------------- */
+      // Product.insertMany(dataProduct);
+      // ProductStat.insertMany(dataProductStat);
       // User.insertMany(dataUser);
     });
   })
